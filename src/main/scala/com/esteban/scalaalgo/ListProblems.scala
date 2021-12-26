48c507038b88c7da7e610f6ef85b373ffb2a6b99
esteban <esteban.marin@adidas.com>
2021-12-26 10:43:16 -0500

    solved K element

--- src/main/scala/com/esteban/scalaalgo/ListProblems.scala
+++ src/main/scala/com/esteban/scalaalgo/ListProblems.scala
@@ -7,15 +7,25 @@ sealed abstract class RList[+T]:
   def tail: RList[T]
   val isEmpty: Boolean
   def ::[S >: T](that: S): RList[S] = new ::(that, this)
+  def apply(index: Int): T
 
 case object RNill extends RList[Nothing]:
   def head = throw new NoSuchElementException
   def tail = throw new NoSuchElementException
   val isEmpty = true
   override def toString: String = "[]"
+  override def apply(index: Int): Nothing = throw new NoSuchElementException
 
 case class ::[+T](override val head: T, override val tail: RList[T]) extends RList[T]:
   override val isEmpty = false
+  override def apply(index: Int): T =
+    @tailrec
+    def recursiveSearch(iteration: Int, remaining: RList[T]): T =
+      if (iteration == index) remaining.head
+      else recursiveSearch(iteration + 1, remaining.tail)
+    if (index < 0) throw new NoSuchElementException
+    else recursiveSearch(0, this)
+
   override def toString: String =
     @tailrec
     def toStringTail(remaining: RList[T], result: String): String =
@@ -27,6 +37,6 @@ case class ::[+T](override val head: T, override val tail: RList[T]) extends RLi
 
 @main def firstMain =
   val nList = 1 :: 2 :: 3 :: RNill
-  println(nList)
+  println(nList(2))
 
 object ListProblems
