b822417c25eb51471330a906f05231a9e0db7301
esteban <esteban.marin@adidas.com>
2021-12-28 14:46:05 -0500

    more

--- src/main/scala/com/esteban/scalaalgo/NumberAndMaths.scala
+++ src/main/scala/com/esteban/scalaalgo/NumberAndMaths.scala
@@ -1,3 +1,22 @@
 package com.esteban.scalaalgo
 
-object NumberAndMaths {}
+import scala.annotation.tailrec
+
+object NumMath:
+  def isPrime(n: Int): Boolean =
+    /*
+    isPrime(3) = 2 > 9 ? = 3
+     */
+    @tailrec
+    def isPrimeTailRec(current: Int): Boolean =
+      if (current > Math.sqrt(Math.abs(n))) true
+      // sigue derecho si no es true
+      else n % current != 0 && isPrimeTailRec(current + 1)
+    // continua la iteracino
+    isPrimeTailRec(2)
+
+@main def numbers =
+  println("-" * 50)
+  println("in numbers")
+  println(NumMath.isPrime(3))
+  println("-" * 50)
