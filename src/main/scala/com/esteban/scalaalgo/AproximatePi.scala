e72b5585d0faec4841dce38f089196cd006f6e3e
esteban <esteban.marin@adidas.com>
2021-12-28 15:53:17 -0500

    pi

---
+++ src/main/scala/com/esteban/scalaalgo/AproximatePi.scala
@@ -0,0 +1,17 @@
+package com.esteban.scalaalgo
+
+import scala.util.Random
+
+object AproximatePi:
+  val random = new Random(System.currentTimeMillis())
+  def aproximatePi(n: Int): Double =
+    val nPointsInside = (1 to n)
+      .map { _ =>
+        val x = random.nextDouble()
+        val y = random.nextDouble()
+        x * x + y * y
+      }
+      .count(distance => distance < 1)
+    nPointsInside * 4.0 / n
+  @main def pi =
+    println(aproximatePi(1000000))
