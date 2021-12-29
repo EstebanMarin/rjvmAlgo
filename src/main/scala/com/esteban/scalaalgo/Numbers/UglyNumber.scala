c0cb5d0146cf115b764a323a711e1222c86788ca
esteban <esteban.marin@adidas.com>
2021-12-28 19:22:31 -0500

    more

--- src/main/scala/com/esteban/scalaalgo/Numbers/UglyNumber.scala
+++ src/main/scala/com/esteban/scalaalgo/Numbers/UglyNumber.scala
@@ -1,23 +1,17 @@
 package com.esteban.scalaalgo.Numbers
 
-object UglyNumber:
-  /*
-    only factors 2,3,5
-   */
-  def uglyNumber_V0(number: Int): Boolean =
-    // List(2, 3, 5).filter(x => number % x == 0).isEmpty
-    val test = List(2, 3, 5).filter(x => number % x == 0)
-    println(test)
-    test.isEmpty
+import scala.annotation.tailrec
 
+object UglyNumber:
   def uglyNumber(number: Int): Boolean =
     def logic(acc: List[Int]): Boolean =
       val set = Set(2, 3, 5)
       acc.filter(x => !set.contains(x)).isEmpty
+    @tailrec
     def uglyTailRec(interation: Int, acc: List[Int]): Boolean =
       if (interation == number) logic(acc)
       else uglyTailRec(interation + 1, if (number % interation == 0) interation :: acc else acc)
-    uglyTailRec(2, Nil)
+    if (number == 1) true else uglyTailRec(2, Nil)
 
 @main def uglyMain =
   println(UglyNumber.uglyNumber(6))
