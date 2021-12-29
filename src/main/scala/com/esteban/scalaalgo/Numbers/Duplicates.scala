759936b996e94f3e20ea1ead2c23661f186ec7f7
esteban <esteban.marin@adidas.com>
2021-12-28 19:47:29 -0500

    more

---
+++ src/main/scala/com/esteban/scalaalgo/Numbers/Duplicates.scala
@@ -0,0 +1,12 @@
+package com.esteban.scalaalgo.Numbers
+
+object Duplicates:
+  def duplictes(list: List[Int]): Int =
+    def duplicatesTailRec(remaing: List[Int], acc: Set[Int]): Int =
+      1
+    1
+
+@main def duplicateMain =
+  println("-" * 50)
+  println("Hello from duplicate")
+  println("-" * 50)
--- src/main/scala/com/esteban/scalaalgo/Numbers/UglyNumber.scala
+++ src/main/scala/com/esteban/scalaalgo/Numbers/UglyNumber.scala
@@ -1,6 +1,14 @@
 package com.esteban.scalaalgo.Numbers
 
 import scala.annotation.tailrec
+object UglyNum:
+  implicit class NRich(number: Int):
+    def uglyNumber: Boolean =
+      @tailrec
+      def uglyTailRec(interation: Int, acc: List[Int]): Boolean =
+        if (interation == number) acc.filter(number => !Set(2, 3, 5).contains(number)).isEmpty
+        else uglyTailRec(interation + 1, if (number % interation == 0) interation :: acc else acc)
+      if (number == 1) true else uglyTailRec(2, Nil)
 
 object UglyNumber:
   def uglyNumber(number: Int): Boolean =
@@ -11,5 +19,8 @@ object UglyNumber:
     if (number == 1) true else uglyTailRec(2, Nil)
 
 @main def uglyMain =
+  import UglyNum.*
+
   println(UglyNumber.uglyNumber(6))
   println(UglyNumber.uglyNumber(14))
+  println(6.uglyNumber)
