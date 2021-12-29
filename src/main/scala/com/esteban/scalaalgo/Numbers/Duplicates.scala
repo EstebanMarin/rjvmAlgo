34e013d9e82e257e93be0eed8b1f67e1866857a5
esteban <esteban.marin@adidas.com>
2021-12-29 10:15:44 -0500

    more

--- src/main/scala/com/esteban/scalaalgo/Numbers/Duplicates.scala
+++ src/main/scala/com/esteban/scalaalgo/Numbers/Duplicates.scala
@@ -2,11 +2,15 @@ package com.esteban.scalaalgo.Numbers
 
 object Duplicates:
   def duplictes(list: List[Int]): Int =
+    def addToSet(acc: Set[Int], head: Int): Set[Int] =
+      if (acc.contains(head)) acc - head else acc + head
     def duplicatesTailRec(remaing: List[Int], acc: Set[Int]): Int =
-      1
-    1
+      if (remaing.isEmpty) acc.toList(0)
+      else duplicatesTailRec(remaing.tail, addToSet(acc, remaing.head))
+    duplicatesTailRec(list, Set())
 
 @main def duplicateMain =
   println("-" * 50)
   println("Hello from duplicate")
+  Duplicates.duplictes(List(1, 1, 3, 3, 4, 4, 5))
   println("-" * 50)
