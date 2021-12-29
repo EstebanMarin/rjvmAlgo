1dcab9a255a9affb0aa9761f0507e7b58af7b733
esteban <esteban.marin@adidas.com>
2021-12-28 19:24:41 -0500

    improved

--- src/main/scala/com/esteban/scalaalgo/Numbers/UglyNumber.scala
+++ src/main/scala/com/esteban/scalaalgo/Numbers/UglyNumber.scala
@@ -4,12 +4,9 @@ import scala.annotation.tailrec
 
 object UglyNumber:
   def uglyNumber(number: Int): Boolean =
-    def logic(acc: List[Int]): Boolean =
-      val set = Set(2, 3, 5)
-      acc.filter(x => !set.contains(x)).isEmpty
     @tailrec
     def uglyTailRec(interation: Int, acc: List[Int]): Boolean =
-      if (interation == number) logic(acc)
+      if (interation == number) acc.filter(number => !Set(2, 3, 5).contains(number)).isEmpty
       else uglyTailRec(interation + 1, if (number % interation == 0) interation :: acc else acc)
     if (number == 1) true else uglyTailRec(2, Nil)
 
