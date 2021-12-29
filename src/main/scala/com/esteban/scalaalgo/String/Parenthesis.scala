2bff01d54016f4f490663f9b1d9801241979344a
esteban <esteban.marin@adidas.com>
2021-12-29 15:07:34 -0500

    more

---
+++ src/main/scala/com/esteban/scalaalgo/String/Parenthesis.scala
@@ -0,0 +1,19 @@
+package com.esteban.scalaalgo.String
+
+import java.lang.reflect.Parameter
+
+object Parenthesis:
+  def hasValidP(string: String): Boolean =
+    val stringList: List[Char] = string.toList
+    def logic(head: Char, acc: Set[Char]): Set[Char] =
+      if (acc.contains(head)) acc - head else acc + head
+    def hasValidTailRec(string: List[Char], acc: Set[Char]): Boolean =
+      if (string.isEmpty) !acc.isEmpty
+      else hasValidTailRec(string.tail, logic(string.head, acc))
+    hasValidTailRec(stringList, Set())
+
+@main def paraMan =
+  println("-" * 50)
+  println(Parenthesis.hasValidP("()"))
+  println(Parenthesis.hasValidP("(("))
+  println("-" * 50)
--- src/main/scala/com/esteban/scalaalgo/String/StringProblems.scala
+++ src/main/scala/com/esteban/scalaalgo/String/StringProblems.scala
@@ -1,19 +1,24 @@
 package com.esteban.scalaalgo.String
 
 import scala.annotation.tailrec
-def countCharacters(s: String): Map[Char, Int] =
-  val sList: List[Char] = s.toList
-  def logic(element: Char, acc: Map[Char, Int]): Map[Char, Int] =
-    if (acc.contains(element)) acc + (element -> (acc.apply(element) + 1))
-    else acc + (element -> 1)
-  @tailrec
-  def countTailRec(remaining: List[Char], acc: Map[Char, Int]): Map[Char, Int] =
-    if (remaining.isEmpty) acc
-    else countTailRec(remaining.tail, logic(remaining.head, acc))
-  countTailRec(sList, Map())
+object StringProblems:
+  def countCharacters(s: String): Map[Char, Int] =
+    val sList: List[Char] = s.toList
+    def logic(element: Char, acc: Map[Char, Int]): Map[Char, Int] =
+      if (acc.contains(element)) acc + (element -> (acc.apply(element) + 1))
+      else acc + (element -> 1)
+    @tailrec
+    def countTailRec(remaining: List[Char], acc: Map[Char, Int]): Map[Char, Int] =
+      if (remaining.isEmpty) acc
+      else countTailRec(remaining.tail, logic(remaining.head, acc))
+    countTailRec(sList, Map())
+
+  def anagram(sa: String, sb: String): Boolean =
+    sa.sorted == sb.sorted
 
 @main def stringMain =
   println("-" * 50)
-  println(StringProblems.countCharacters("Scala"))
-  println(StringProblems.countCharacters("Esteban"))
+  // println(StringProblems.countCharacters("Scala"))
+  // println(StringProblems.countCharacters("Esteban"))
+  println(StringProblems.anagram("stressed", "stressed"))
   println("-" * 50)
