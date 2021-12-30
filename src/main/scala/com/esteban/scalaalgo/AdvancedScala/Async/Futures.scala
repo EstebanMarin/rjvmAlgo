2359cac6cf3ce21eff8c4339c761e99dc246f596
esteban <esteban.marin@adidas.com>
2021-12-30 17:23:45 -0500

    more

--- src/main/scala/com/esteban/scalaalgo/AdvancedScala/Async/Futures.scala
+++ src/main/scala/com/esteban/scalaalgo/AdvancedScala/Async/Futures.scala
@@ -3,21 +3,30 @@ package com.esteban.scalaalgo.AdvancedScala.Async
 import scala.concurrent.ExecutionContext
 import java.util.concurrent.Executors
 import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.ExecutionContext
 import scala.util.{ Failure, Success }
 import java.util.concurrent.ExecutorService
+import scala.util.Try
 
 object Futures:
   def meainingofLife(): Int =
     Thread.sleep(1000)
     42
+  val executors = Executors.newFixedThreadPool(4)
+  given executionContext: ExecutionContext =
+    ExecutionContext.fromExecutorService(executors)
 
-  val excutionContext =
-    ExecutionContext.fromExecutorService(Executors.newFixedThreadPool(4))
+  val aFuture: Future[Int] = Future(meainingofLife())
+  val futureResult: Option[Try[Int]] = aFuture.value
 
-// ExecutionContext.fromExecutorService())
+  aFuture.onComplete {
+    case Success(value) => println(s"$value")
+    case Failure(ex) => println(s"$ex")
+  }
 
-@main def futuresMain =
-  println("-" * 50)
-  println("-" * 50)
+  @main def futuresMain =
+    println("-" * 50)
+    println(futureResult)
+    Thread.sleep(2000)
+    executors.shutdown()
+    println("-" * 50)
