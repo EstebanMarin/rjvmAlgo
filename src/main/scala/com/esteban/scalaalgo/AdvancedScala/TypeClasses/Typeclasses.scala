84584155d5c5b154e438a10cc491ec4275a9ba5c
esteban <esteban.marin@adidas.com>
2022-01-17 19:07:08 -0500

    more

--- src/main/scala/com/esteban/scalaalgo/AdvancedScala/TypeClasses/Typeclasses.scala
+++ src/main/scala/com/esteban/scalaalgo/AdvancedScala/TypeClasses/Typeclasses.scala
@@ -1,6 +1,31 @@
 package com.esteban.scalaalgo.AdvancedScala.TypeClasses
 
 object Typeclasses:
+  trait HTMLWritable:
+    def toHtml: String
+
+  case class User(
+      name: String,
+      age: Int,
+      email: String,
+    ) extends HTMLWritable:
+    override def toHtml = s"<div>$name $age yo <a href=$email/></div>"
+
+// Part 1 - Type class definition
+  trait HTMLSerializer[T]:
+    def serialized(value: T): String
+
+  // Part 2 - Type class instance for the supported type
+  given userSerializer: HTMLSerializer[User] with
+    override def serialized(value: User): String =
+      val User(name, age, email) = value
+      s"<div>$name $age yo <a href=$email/></div>"
+
+
+  val bobUser = User("bob", 46, "bob@com.com")
+  val bob: String = userSerializer.serialized(bobUser)
+
   @main def typeMain =
     println("-" * 50)
+    println(bob)
     println("-" * 50)
