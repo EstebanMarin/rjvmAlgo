dec463b541f5f7a44ada3cb8a7b841343d24a298
EstebanMarin <estebanmarin002@gmail.com>
2022-01-26 13:23:09 -0500

    more

--- src/main/scala/com/esteban/scalaalgo/AdvancedScala/variance/Variance.scala
+++ src/main/scala/com/esteban/scalaalgo/AdvancedScala/variance/Variance.scala
@@ -1,5 +1,26 @@
 package com.esteban.scalaalgo.AdvancedScala.variance
 
-object Variance {
-  
-}
+object Variance:
+  class Animal
+  case class Dog(name: String) extends Animal
+
+  class MyList[+A]
+
+  val lassie = Dog("Lassie")
+  val hachi = Dog("Hachi")
+  val listOfAnimals: List[Animal] = List(lassie, hachi)
+
+  trait Vet[-A]:
+    def heal(animal: A): Boolean
+
+  val myVet: Vet[Dog] = new Vet[Animal]:
+    override def heal(animal: Animal) = true
+
+  val healLassie = myVet.heal(lassie)
+
+/*
+    Rule of thumb:
+        - if your type PRODUCES or Retreives a value. Then it should be COVARIANT
+        - if your type CONSUMES or ACTS ON a value CONTRAVARIANT
+        - Otherwise INVARIANT
+ */
