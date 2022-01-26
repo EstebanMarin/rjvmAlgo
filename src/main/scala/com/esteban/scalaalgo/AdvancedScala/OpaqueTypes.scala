e793f9a5f3982e124f365fbe0c0f400597157676
EstebanMarin <estebanmarin002@gmail.com>
2022-01-26 18:15:29 -0500

    more

--- src/main/scala/com/esteban/scalaalgo/AdvancedScala/OpaqueTypes.scala
+++ src/main/scala/com/esteban/scalaalgo/AdvancedScala/OpaqueTypes.scala
@@ -1,9 +1,15 @@
 package com.esteban.scalaalgo.AdvancedScala
 
+import com.esteban.scalaalgo.AdvancedScala.OpaqueTypes.SocialNetwork.Name
+
 object OpaqueTypes:
   object SocialNetwork:
     // some data struncture = " domain"
     opaque type Name = String
+    object Name:
+      def apply(stg: String): Name = stg
+    extension (name: Name)
+      def length: Int = name.length
     // inside Name <-> String
     def addFriend(person: Name, person2: Name): Boolean =
       person.length == person2.length
@@ -21,9 +27,11 @@ object OpaqueTypes:
     val Green: Color = 0x00ff000
     val Blue: Color = 0x00000ff
     val halfTransperancy: ColorFilter = 0x88
+   
+  import Graphics.*
+  val aName: Name = Name("Esteban")
+  val nameLength: Int = aName.length
 
-    import Graphics.*
-
-    @main def opaceMain =
-      println("-" * 50)
-      println("-" * 50)
+  @main def opaceMain =
+    println("-" * 50)
+    println("-" * 50)
