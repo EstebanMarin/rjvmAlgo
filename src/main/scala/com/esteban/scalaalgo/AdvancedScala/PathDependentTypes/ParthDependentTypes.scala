382f3bb413c6f92481817692512209bc9ebe05f3
EstebanMarin <estebanmarin002@gmail.com>
2022-01-26 17:48:07 -0500

    more

--- src/main/scala/com/esteban/scalaalgo/AdvancedScala/PathDependentTypes/ParthDependentTypes.scala
+++ src/main/scala/com/esteban/scalaalgo/AdvancedScala/PathDependentTypes/ParthDependentTypes.scala
@@ -1,6 +1,7 @@
 package com.esteban.scalaalgo.AdvancedScala.PathDependentTypes
 
 import com.esteban.scalaalgo.AdvancedScala.PathDependentTypes.ParthDependentTypes.Outer
+import com.esteban.scalaalgo.AdvancedScala.PathDependentTypes.ParthDependentTypes.StringRecord
 
 object ParthDependentTypes:
   class Outer:
@@ -8,6 +9,7 @@ object ParthDependentTypes:
       object InnerObject
       type InnerType
       def process(arg: Inner) = println(arg)
+      def processGeneral(arg: Outer#Inner) = println(arg)
 
   val outer = new Outer
   val inner = new outer.Inner 
@@ -20,9 +22,34 @@ object ParthDependentTypes:
   val innerB = new outerB.Inner
 
 
-//   outerA.process(innerB)
+//   outerA.process(innerB) // compilation error
   outerA.process(innerA)
-  
+  outer.process(inner)
+
+  //parent type: Outer#Inner
+  outerA.processGeneral(innerA)
+  outerA.processGeneral(innerB) // outer.inner <: Outer#Inner
+  //type level programming
+  trait Record:
+      type Key
+      def defaultValue: Key
+
+  class StringRecord extends Record:
+      override type Key = String
+      override def defaultValue = ""
+
+  class IntRecord extends Record:
+      override type Key = Int
+      override def defaultValue = 0
+
+  def getDefaultValue(record: Record): record.Key = record.defaultValue
+
+  val aString: String = getDefaultValue(new StringRecord)
+  val anInt: Int = getDefaultValue(new IntRecord)
+
+  // function with dependent types
+  val getIdentifier: (record: Record) => record.Key = getDefaultValue
+
   @main def PathMain =
     println("-" * 50)
     println("-" * 50)
