8b8f9b09beafe3c35509fa7d8eb0286185e43037
EstebanMarin <estebanmarin002@gmail.com>
2022-01-26 18:53:39 -0500

    more

--- src/main/scala/com/esteban/scalaalgo/AdvancedScala/SelfTypes/Seftypes.scala
+++ src/main/scala/com/esteban/scalaalgo/AdvancedScala/SelfTypes/Seftypes.scala
@@ -1,8 +1,6 @@
 package com.esteban.scalaalgo.AdvancedScala.SelfTypes
 
-object Seftypes :
-   @main def selfTypesMain =
+object Seftypes:
+  @main def selfTypesMain =
     println("-" * 50)
     println("-" * 50)
- 
-
